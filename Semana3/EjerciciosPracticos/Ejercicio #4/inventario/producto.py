47362afe8104ab1990d22c2204bad0366947b758
SteMt323 <stevenmt@uamv.edu.ni>
2025-03-30 19:20:28 -0600

    Encapsulando atributos de la clase Producto, semana3 - ejercicio4

--- Semana3/EjerciciosPracticos/Ejercicio #4/inventario/producto.py
+++ Semana3/EjerciciosPracticos/Ejercicio #4/inventario/producto.py
@@ -1,10 +1,16 @@
 
 class Producto():
     def __init__(self, codigo: int, nombre: str, precio: float, cantidad_stock: int):
-        self.codigo = codigo
+        self._codigo = codigo
         self.nombre = nombre
         self.precio = precio
         self.cantidad_stock = cantidad_stock
         
+    @property
+    def codigo(self):
+        return self._codigo
     
+    @codigo.setter
+    def codigo(self, nuevo_codigo):
+        print("El código nu puede ser modificado")
         
