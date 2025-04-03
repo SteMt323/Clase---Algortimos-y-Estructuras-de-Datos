271e67307091e39968c9739e73d4c027d2b91645
SteMt323 <stevenmt@uamv.edu.ni>
2025-04-03 08:30:40 -0600

    Eliminando ejercicio 4 (todo esta mal)

--- Semana3/EjerciciosPracticos/Ejercicio #4/helpers/validations.py
+++
@@ -1,9 +0,0 @@
-def int_evaluation(num):
-    if num is None:
-        return True
-    return isinstance(num, int)
-    
-def float_evaluation(num):
-    if num is None:
-        return True
-    return isinstance(num, float)
